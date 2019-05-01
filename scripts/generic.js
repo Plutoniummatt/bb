6a46019a8bd9f4147368595cbaad5f04ce54ce87
Matthew Peck <peck@squareup.com>
2019-05-01 01:24:25 -0700

    Brains

--- scripts/generic.js
+++ scripts/generic.js
@@ -1,12 +1,22 @@
 module.exports = robot => {
   // bb start
   robot.respond(/\s+start/, res => {
-    res.send("Starting badminton session now. Enjoy!");
+    if (robot.brain.get('started')) {
+      res.send("People are already playing! Get on with it.");
+    } else {
+      robot.brain.set('started', true);
+      res.send("Starting badminton session now. Enjoy!");
+    }
   });
 
   // bb stop
   robot.respond(/\s+stop/, res => {
-    res.send("Ending badminton session. You’ve played X hours and Y mins.");
+    if (robot.brain.get('started')) {
+      robot.brain.set('started', false);
+      res.send("Ending badminton session. You’ve played X hours and Y mins.");
+    } else {
+      res.send("Can't stop what aint running, capn'");
+    }
   });
 
   // bb help
