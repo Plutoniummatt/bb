8f5c06f505736ba3c0d49a02dbf0445df69f6ef8
Jonathan Chay <jonchay@squareup.com>
2019-05-01 19:45:41 -0700

    Add court duration for start delay

--- scripts/court.js
+++ scripts/court.js
@@ -1,4 +1,6 @@
 const moment = require("moment");
+// by default 45 minutes is considered "in an hour", increase the threshold
+moment.relativeTimeThreshold('m', 50);
 
 // Description:
 // <description of the scripts functionality>
@@ -16,6 +18,7 @@ const moment = require("moment");
 //   <optional notes required for the script>
 
 const { COURTS_REDIS_KEY } = require('./common/constants');
+const COURT_DURATION = 45; // minutes
 
 function humanizeCourtWithPlayers(courtNumber, args) {
   const players = args.slice(0);
@@ -41,30 +44,50 @@ function parseMatches(matches) {
   }
 }
 
-module.exports = robot => {
-  function getAllCourts() {
-    return robot.brain.get(COURTS_REDIS_KEY) || {};
-  };
+function getAllCourts(robot) {
+  return robot.brain.get(COURTS_REDIS_KEY) || {};
+};
 
-  function setAllCourts(court) {
-    return robot.brain.set(COURTS_REDIS_KEY, court);
+function setAllCourts(robot, court) {
+  return robot.brain.set(COURTS_REDIS_KEY, court);
+}
+
+
+// group A 0
+// group B 0 # 0 + 45 mins
+// group C 45 # 45 + 45 mins
+
+// group A 23
+// group B 0 # 23 + 45
+
+// group A 23
+// group B 10 # 23 + 45 + 10
+
+function addCourt(robot, number, players, randoms, delayTime = 0) {
+  const courts = getAllCourts(robot);
+  const courtKey = `court_${number}`;
+  courts[courtKey] = courts[courtKey] || [];
+  const existingQueue = courts[courtKey];
+  const lastCourt = existingQueue[existingQueue.length - 1] || {};
+
+  const postponeStart = lastCourt.startAt
+    ? (COURT_DURATION + delayTime)
+    : delayTime;
+
+  const startAt = moment(lastCourt.startAt).add(postponeStart, 'minutes');
+
+  const courtQueue = {
+    players,
+    randoms,
+    startAt
   }
 
-  function addCourt(number, players, randoms, delayTime = 0) {
-    const courts = getAllCourts();
+  courts[courtKey].push(courtQueue);
+  setAllCourts(robot, courts);
+  return courtQueue;
+};
 
-    // startAt = moment().add(delayTime, 'minutes');
-    startAt = moment().add(delayTime, 'minutes');
-    courts[`court_${number}`] = courts[`court_${number}`] || [];
-    courts[`court_${number}`].push({
-      players,
-      randoms,
-      startAt,
-    })
-
-    setAllCourts(courts);
-  };
-
+module.exports = robot => {
   // bb ct|court|crt <court_number> <names>... <delay_time>
   robot.respond(/\s+(?:ct|court|crt)\s+(\d*)\s+([\w\d].*)/i, res => {
     const {
@@ -78,24 +101,22 @@ module.exports = robot => {
       return;
     }
 
-    const courtDescription = humanizeCourtWithPlayers(courtNumber, players);
-    const expiringTimeDescription = delayTime === null
-      ? ''
-      : ` starting in ${delayTime} minutes`;
-
-    addCourt(
+    const newCourt = addCourt(
+      robot,
       courtNumber,
       players,
       false,
       delayTime
     );
 
-    res.send(`${courtDescription} ${expiringTimeDescription}`);
+    const courtDescription = humanizeCourtWithPlayers(courtNumber, players);
+    const fromNowDescription = ` starting ${moment(newCourt.startAt).fromNow()}`;
+    res.send(`${courtDescription} ${fromNowDescription}`);
   });
 
   // bb ct|court|crt status
   robot.respond(/\s+(?:ct|court|crt)\s+status$/i, res => {
-    const courts = getAllCourts();
+    const courts = getAllCourts(robot);
     res.send(`Beep boop... gathering court status...`);
     res.send(`\`\`\`${JSON.stringify(courts, null, 2)}\`\`\``);
   });
